680540572fafbc5895bf924606cbb13756506347
Marco <marcoledezmacordero09@gmail.com>
2017-03-21 03:26:12 -0600

    Medea's landing page
    I took Keylin's version, made some changes and added the module of the
    synopsis and the teaser

---
+++ js/main.js
@@ -0,0 +1,39 @@
+jQuery(document).ready(function($) {
+    
+    // Functions
+    // Toggles the visibility of the modals
+    var toggleVisibility = function toggleVisibility() {
+        if(!$('#teaser').hasClass('hidden')) {
+            $('.opacity').addClass('hidden');
+            $('#teaser').addClass('hidden');            
+        }
+        if(!$('#synopsis').hasClass('hidden')) {
+            $('.opacity').addClass('hidden');
+            $('#synopsis').addClass('hidden');            
+        }
+    }
+
+    // When you click the synopsis button, removes the class hidden of the modal
+    $('#btnSynopsis').click(function(){
+        $('.opacity').removeClass('hidden')
+        $('#synopsis').removeClass('hidden');
+    });
+
+    // When you click the teaser button, removes the class hidden of the modal
+    $('#btnTeaser').click(function(){
+        $('.opacity').removeClass('hidden')
+        $('#teaser').removeClass('hidden');
+    })
+
+    
+
+    // When you click the arrow, hides the modal
+    $('.imgBehind').click(function(){
+        toggleVisibility();
+    })
+
+    // When you click the opacity the modal hides
+    $('.opacity').click(function(){
+        toggleVisibility();
+    })
+});
